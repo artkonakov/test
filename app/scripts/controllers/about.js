e533701dba229d5b4733869a9ae092aab6d8af99
Артём Конаков < >
2016-04-21 18:23:14 +0300

    second http req

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -10,11 +10,26 @@
 angular.module('cupon2App')
     .controller('AboutCtrl', ['$scope', '$routeParams', '$http',
   function ($scope, $routeParams, $http) {
-            $http.get('/' + $routeParams.itemId + '.json').success(function (data) {
-                $scope.item = data;
+            var currentitem = $routeParams.itemId;
+            var geturl = 'https://api.backendless.com/v1/data/items?where=linkid%20%3D%20%27' + currentitem + '%27';
+            var req = {
+             method: 'GET',
+             url: geturl,
+             headers: {
+               'application-Id': "31877D2E-F358-A666-FF42-B6EF6705A800",
+               'secret-key': "2C7C2056-6BFF-46B7-FFB8-EA1309821000"
+             }
+            };
+            $http(req).success(function (data) {
+             $scope.singleitem = data.data[0];
             });
-            $scope.api = 'B495C09D-FD46-8BB8-E71A-457FF952CEB8';
-            $scope.url = '';
+
+
+
+
+
+            var api = 'B495C09D-FD46-8BB8-E71A-457FF952CEB8';
+            var url = '';
 
             $scope.send = function (usertel) {
                 $scope.url = 'http://sms.ru/sms/send?api_id=' + $scope.api + '&to=' + usertel + '&text=' + $scope.item.sms;
